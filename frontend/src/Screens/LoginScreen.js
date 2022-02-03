9298e07b8bc111b0b4c0d493e3d2a4bc22401812
shivanshgupta340 <shivanshgupta34@gmail.com>
2022-02-03 22:39:42 +0530

    register link update

--- frontend/src/Screens/LoginScreen.js
+++ frontend/src/Screens/LoginScreen.js
@@ -109,7 +109,7 @@ const LoginScreen = ({history}) =>{
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
