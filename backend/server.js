a48602700403dab95aaf26a93734d1c2982efc7f
shivanshgupta340 <shivanshgupta34@gmail.com>
2022-02-03 20:25:45 +0530

    heroku command

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV==='development'){
 }
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+	res.sendFile(path.join(process.cwd(), '/frontend/build/index.html'));
 });
 
 app.use("/api/products", productRoutes);
